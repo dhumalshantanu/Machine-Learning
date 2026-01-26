702240dc8642e3266d11399ab4e9dfd84b1cfb4d
dhumalshantanu <sdhumal2905@gmail.com>
2026-01-26 15:11:37 +0530

    Added Performance metrix file

---
+++ P13-PerformaceMetrics.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+y_true = [1, 1, 0, 1, 0, 0, 0, 1, 1] # What actully happned
+
+y_predicted = [1, 0, 1, 0, 0, 0, 1, 1, 1] # What model predicted
+
+print("Accuracy : ", accuracy_score(y_true, y_predicted))
+print("Precision : ", precision_score(y_true, y_predicted))
+print("Recall : ", recall_score(y_true, y_predicted))
+print("F1 Score : ", f1_score(y_true, y_predicted))
+
+
