a4b4f17410d4bd4d027827dec7899055f10a0f1b
dhumalshantanu <sdhumal2905@gmail.com>
2026-01-26 15:39:41 +0530

    Added MSE and MAE in the code

--- P13-PerformaceMetrics.py
+++ P13-PerformaceMetrics.py
@@ -1,5 +1,7 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import numpy as np
 
 y_true = [1, 1, 0, 1, 0, 0, 0, 1, 1] # What actully happned
 
@@ -13,3 +15,12 @@ print("F1 Score : ", f1_score(y_true, y_predicted))
 cm = confusion_matrix(y_true, y_predicted)
 print("Confusion Matrix : ")
 print(cm)
+
+mae = mean_absolute_error(y_true, y_predicted)
+mse = mean_squared_error(y_true, y_predicted)
+rmse = np.sqrt(mse)
+
+print("MAE : On average off by : ", mae)
+print("MSE : Squared Mistake Value : ", mse)
+print("RMSE : Final Realistic Error : ", rmse)
+
