7bf71fbe279d5f4872c8a977a94d016cf66c39c2
dhumalshantanu <sdhumal2905@gmail.com>
2026-01-26 15:20:32 +0530

    Added confusion Matrix code

--- P13-PerformaceMetrics.py
+++ P13-PerformaceMetrics.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
 
 y_true = [1, 1, 0, 1, 0, 0, 0, 1, 1] # What actully happned
 
@@ -9,4 +10,6 @@ print("Precision : ", precision_score(y_true, y_predicted))
 print("Recall : ", recall_score(y_true, y_predicted))
 print("F1 Score : ", f1_score(y_true, y_predicted))
 
-
+cm = confusion_matrix(y_true, y_predicted)
+print("Confusion Matrix : ")
+print(cm)
